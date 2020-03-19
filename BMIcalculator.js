07f5fb7a80b708ce4f28c7c330566e187a4cd41a
londo <london.man77@gmail.com>
2020-03-20 02:41:23 +0530

    remove trash

--- BMIcalculator.js
+++
@@ -1,9 +0,0 @@
-function calculate1() {
-    var wt = parseFloat(document.getElementById("wt").value);
-    var ht = parseFloat(document.getElementById("ht").value);
-    var bmi = wt / (ht * ht);
-    var res = document.querySelector("#result");
-    res.textContent = wt;
-}
-var button = document.querySelector("#calculate");
-button.addEventListener("click", calculate1);
