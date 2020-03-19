3ef5a64ec75465a0b0a4bff31556b4a841e1c5a6
Akshay <nerdypepper@tuta.io>
2020-03-20 00:38:47 +0530

    add header + basic testing on index

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
+export default function Index() {
+  return (
+    <React.Fragment>
+    <Header/ >
+      <Container>
+        <Box my={2}>
+          {[...new Array(5000)]
+            .map(
+              () => `pepper `,
+            )
+            .join('\n')}
+        </Box>
+      </Container>
+  </React.Fragment>
+  )
+}
