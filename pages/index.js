76a0137f670db0df4a10e20e577bef3a473ef710
Akshay <nerdypepper@tuta.io>
2020-03-20 01:54:59 +0530

    add welcome component

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 import Header from "../components/Header";
 import Footer from "../components/footer";
+import Welcome from "../components/Welcome";
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function Index() {
   return (
     <React.Fragment>
-    <Header/ >
-      <Container>
-        <Box my={2}>
+      <CssBaseline />
+      <Header/ >
+        <Container>
+          <Welcome />
           {[...new Array(5000)]
-            .map(
-              () => `pepper `,
-            )
-            .join('\n')}
-        </Box>
-      </Container>
-      <Footer/ >
-  </React.Fragment>
-  )
+              .map(
+                () => `pepper `,
+              )
+              .join('\n')}
+        </Container>
+        <Footer/ >
+        </React.Fragment>
+        )
 }
