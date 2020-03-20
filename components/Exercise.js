d2932efd05d40cc30ddb38758847559d71eeaed1
Akshay <nerdypepper@tuta.io>
2020-03-20 13:04:29 +0530

    move images to CDN, redo cards

--- components/Exercise.js
+++ components/Exercise.js
@@ -1,87 +1,88 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Grid from '@material-ui/core/Grid';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 300,
-    height: 350,
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const tileData = [
   {
     title: "Meditation",
-    author: "Shaibro",
-    date: "March 19, 2020",
+    author: "Martha Cabrerra",
+    time: "11 minutes",
+    image: "https://res.cloudinary.com/dc0qqhiaz/image/upload/v1584688758/meditation_rgt0i5.jpg",
   },
   {
-    title: "Meditation",
-    author: "Shaibro",
-    date: "March 19, 2020",
+    title: "Yoga",
+    author: "Arnold S.",
+    time: "5 minutes",
+    image: "https://res.cloudinary.com/dc0qqhiaz/image/upload/v1584689487/Yoga_dmghhe.jpg",
   },
   {
-    title: "Meditation",
-    author: "Shaibro",
-    date: "March 19, 2020",
+    title: "Back Extension",
+    author: "Marilyn Monroe",
+    time: "9 minutes",
+    image: "https://res.cloudinary.com/dc0qqhiaz/image/upload/v1584689741/back_qxbkaj.jpg",
   },
   {
-    title: "Meditation",
-    author: "Shaibro",
-    date: "March 19, 2020",
+    title: "Cross Crunches",
+    author: "Kensington",
+    time: "10 minutes",
+    image: "https://res.cloudinary.com/dc0qqhiaz/image/upload/v1584689544/crunches_kcp2fp.jpg",
   },
   {
-    title: "Meditation",
-    author: "Shaibro",
-    date: "March 19, 2020",
+    title: "Zumba",
+    author: "Nagaraju",
+    time: "12 minutes",
+    image: "https://res.cloudinary.com/dc0qqhiaz/image/upload/v1584689485/zumba_p1p03i.jpg",
   },
   {
-    title: "Meditation",
-    author: "Shaibro",
-    date: "March 19, 2020",
+    title: "Martial Arts",
+    author: "Nagoor Babu",
+    time: "7 minutes",
+    image: "https://res.cloudinary.com/dc0qqhiaz/image/upload/v1584689484/martial_ovjyvf.jpg",
   },
 ]
 
-export default function E() {
-  const classes = useStyles();
-
+export default function Exercise() {
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
-          <ListSubheader component="div">Excercises</ListSubheader>
-        </GridListTile>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img}>
-            <img src="/static/meditation.jpg" alt={tile.title} height="350" width="300"/>
-            <GridListTileBar
-              title={tile.title}
-              subtitle={<span>by: {tile.author}</span>}
-              actionIcon={
-                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-
+      <Grid container spacing={3}>
+        {tileData.map((tile, idx) => (
+          <Grid item xs={12} sm={6} md={3} key={idx}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                component="img"
+                image={tile.image}
+                title="Meditation"
+              />
+                </CardActionArea>
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {tile.title}
+                  </Typography>
+                  <Typography variant="body2" component="p" color="textSecondary">
+                    {tile.author} · {tile.time}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">
+                    Train Now
+                  </Button>
+                </CardActions>
+              </Card>
+          </Grid>
         ))}
-      </GridList>
-    </div>
+      </Grid>
   );
 }
